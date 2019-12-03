c46ebfc4b856a4bdd6d665a6aaffa17ec08ca38e
Kathrin Kaiser <kaiserk@tl-agent1.mathematik.uni-kl.de>
2019-12-03 11:24:28 +0100

    diffiehellman sicherer, bsgs implementiert, angriff fehlerhaft

--- angriff.jl
+++ angriff.jl
@@ -1,4 +1,15 @@
-function angriff(p::BigInt,P::NfAbsOrdIdl,A::NfAbsOrdIdl,Mneu::NfAbsOrdIdl,B::NfAbsOrdIdl)
+function encode_klein(p::BigInt,m::BigInt)
+#include find.jl
+	M=findnonprincipal(BigInt(p),m*10^3,m*10^4)
+	return Hecke.reduce_ideal(M)
+end
+
+function decode_klein(M::NfAbsOrdIdl)
+	m=divrem(M.gen_one,10^3)[1]
+	return m
+end
+
+function angriff(p::BigInt,P::NfAbsOrdIdl,A::NfAbsOrdIdl,C::NfAbsOrdIdl,B::NfAbsOrdIdl)
 	k,a=quadratic_field(-p)
 	c,mc=class_group(order(A))
 	n=BigInt(order(c))
@@ -7,17 +18,18 @@ function angriff(p::BigInt,P::NfAbsOrdIdl,A::NfAbsOrdIdl,Mneu::NfAbsOrdIdl,B::Nf
 	println("l=",l)
 	a=fmpz(n/l)
 	println("a=",a)
-	if Mneu==0
+	if C==0
 		return Hecke.power_class(P,a)==A
 	else
-		return Mneuneu=Hecke.reduce_ideal(Mneu*(Hecke.power_class(B,a))),l
+		return Mneuneu=Hecke.reduce_ideal(C*(Hecke.power_class(B,a))),l
 	end
+
 end	
 
 function testmyangriff()
 	m=BigInt(17)
 	println("m=",m)
-	p=BigInt(nextprime(1000))
+	p=BigInt(nextprime(10000000000))
 	k,a=quadratic_field(-p)
 	c,mc=class_group(k)
 	println("c=",c)
@@ -27,13 +39,73 @@ function testmyangriff()
 	a,A=diffiehellmanA(P)
 	println("a=",a)
 	println("A=",A)
-	M=encode(p,m)
+	M=encode_klein(p,m)
 	println("M=",M)
 	C,B=elgamalB(M,A,P)
 	println("C=",C)
-	Mneu=elgamalA(C,B,a)
-	println("Mneu=",Mneu)
-	Mneuneu,l=angriff(p,P,A,Mneu,B)
+	#Mneu=elgamalA(C,B,a)
+	#println("Mneu=",Mneu)
+	Mneuneu,l=angriff(p,P,A,C,B)
 	println("Mneuneu=",Mneuneu)
-	return M==Mneuneu
+	return Hecke.reduce_ideal(M*inv(Mneuneu))
 end
+
+function bsgs(p::BigInt,P::NfAbsOrdIdl,A::NfAbsOrdIdl,C::NfAbsOrdIdl,B::NfAbsOrdIdl)
+	k,a=quadratic_field(-p)
+	c,mc=class_group(order(A))
+	n=BigInt(order(c))
+	m=BigInt(ceil(sqrt(n)))
+	g=[]
+	h=[A]
+	d=0
+	l=0
+	x=Hecke.power_class(P,fmpz(-m))
+	for i=1:m
+		push!(g,Hecke.power_class(P,fmpz(i-1)))
+	end
+	for i=1:m
+		push!(h,h[i]*x)
+		#println(i)
+		#println(h)
+		for j=1:m
+			println(Hecke.reduce_ideal(h[i+1]*inv(g[j])))
+			if Hecke.reduce_ideal(h[i+1]*inv(g[j])).gen_one==1
+				d=j
+				l=i+1
+				println()
+				break
+			end
+		end
+	end
+	a=d+l*m
+	#if C==0
+		return a
+	#else
+	#	return Mneuneu=Hecke.reduce_ideal(C*(Hecke.power_class(B,fmpz(a))))
+	#end
+end
+
+function testbsgs()
+	m=BigInt(17)
+	println("m=",m)
+	p=BigInt(nextprime(10000000000000))
+	k,a=quadratic_field(-p)
+	c,mc=class_group(k)
+	println("c=",c)
+	P=mc(c[1])
+	println("p=",p)
+	println("P=",P)
+	a,A=diffiehellmanA(P)
+	println("a=",a)
+	println("A=",A)
+	M=encode_klein(p,m)
+	println("M=",M)
+	C,B=elgamalB(M,A,P)
+	println("C=",C)
+	#Mneu=elgamalA(C,B,a)
+	#println("Mneu=",Mneu)
+	a=bsgs(p,P,A,C,B)
+	println("a=",a)
+	#return Hecke.reduce_ideal(M*inv(Mneuneu))
+end
+				
--- diffiehellman.jl
+++ diffiehellman.jl
@@ -6,19 +6,28 @@ function diffiehellman0(m::BigInt)
 		p=next_prime(10*p)
 	end
 	k,a=quadratic_field(-p)
-	P=findnonprincipal(BigInt(p),BigInt(2),BigInt(100000))
+	P=findnonprincipal(BigInt(p),BigInt(floor(sqrt(p))),BigInt(100000*floor(sqrt(p))))
 	return p,P
 end
 
 function diffiehellmanA(P::NfAbsOrdIdl)
-	a=abs(rand(10000000:fmpz(10)^60))
-	A=Hecke.power_class(P,fmpz(a))
+	A=P
+	a=0
+	while Hecke.reduce_ideal(P*inv(A)).gen_one==1 || Hecke.reduce_ideal(A).gen_one==1
+		println(1)
+		a=abs(rand(10000000:fmpz(10)^60))
+		A=Hecke.power_class(P,fmpz(a))
+	end
 	return a,A
 end
 
 function diffiehellmanB(P::NfAbsOrdIdl)
-	b=abs(rand(10000000:fmpz(10)^60))
-	B=Hecke.power_class(P,fmpz(b))
+	B=P
+	b=0
+	while Hecke.reduce_ideal(P*inv(B)).gen_one==1 || Hecke.reduce_ideal(B).gen_one==1
+		b=abs(rand(10000000:fmpz(10)^60))
+		B=Hecke.power_class(P,fmpz(b))
+	end
 	return b,B
 end
 
