adecf3b4e2683c2de6ad31a543a1a0ea307bae53
Kathrin Kaiser <kaiserk@tl-agent3.mathematik.uni-kl.de>
2020-01-08 10:03:32 +0100

    korrektur formalg

--- formalg.jl
+++ formalg.jl
@@ -1,13 +1,15 @@
-function reduce(f::Array{Int64,1})
+function formreduce(f::Array{Float64,1})
 	a=f[1]
 	b=f[2]
 	c=f[3]
 	#D=b*b-4*a*c
-	while b<=(-a) || a<b
-		r=Int64(b%(2+a))
-		q=Int64((b-r)/(2+a))
-		r=r-2*a
-		q=q+1
+	if b<=(-a) || a<b
+		r=Int64(b%(2*a))
+		q=Int64((b-r)/(2*a))
+		if r>a
+			r=r-2*a
+			q=q+1
+		end
 		c=c-((b+r)*q)/2
 		b=r
 	end
@@ -16,10 +18,12 @@ function reduce(f::Array{Int64,1})
 		x=a
 		a=c
 		c=x
-		r=Int64(b%(2+a))
-		q=Int64((b-r)/(2+a))
-		r=r-2*a
-		q=q+1
+		r=Int64(b%(2*a))
+		q=Int64((b-r)/(2*a))
+		if r>a
+			r=r-2*a
+			q=q+1
+		end
 		c=c-((b+r)*q)/2
 		b=r
 	elseif a==c && b<0
@@ -27,6 +31,27 @@ function reduce(f::Array{Int64,1})
 	end
 	return [a;b;c]
 end
+#|b|<=a<=c,c>0 if a=|b| or a=c then b>0
+
+#function testformreduce()
+#	a=rand(1:1000)	
+#	c=rand(-100:100)
+#	d=rand(1:1000)
+#	e=4*a*c-d
+#	while e<0
+#		d=rand(1:1000)
+#		e=4*a*c-d	
+#	end
+#	b=Int64(floor(sqrt(e)))
+#	f=[a;b;c]
+#	[x;y;z]=formreduce(f)
+#	i=((-a)<=b || b<=a) && a<=c
+#	j=true
+#	if a==-b || a==b || a==c
+#		j=b>0
+#	end
+#	return i&&j
+#end 
 
 function compose(f::Array{Int64,1},g::Array{Int64,1})
 	a1=f[1]
@@ -62,7 +87,7 @@ function compose(f::Array{Int64,1},g::Array{Int64,1})
 	b3=b2+2*v2*r
 	a3=v1*v2
 	c3=(c2*d1+r*(b2+v2*r))/v1
-	return reduce([a3;b3;c3])
+	return formreduce([a3;b3;c3])
 end
 
 function parteucl(a::Int64,b::Int64,L::Int64)
@@ -72,8 +97,11 @@ function parteucl(a::Int64,b::Int64,L::Int64)
 	v3=b
 	z=0
 	while abs(v3)>L
-		t2=v-gcdx(v3,t3)[2]*v2
-		v=v2#d=v3
+		t3=d%v3
+		q=Int64((d-t3)/v3)
+		t2=v-q*v2
+		v=v2
+		d=v3
 		v2=t2
 		v3=t3
 		z+=1
@@ -85,27 +113,28 @@ function parteucl(a::Int64,b::Int64,L::Int64)
 	return (z,d)
 end
 
-function nudupl(f::Array{Int64,1})
+function nudupl(f::Array{Int64,1})	
 	a=f[1]
 	b=f[2]
 	c=f[3]
 	D=b*b-4*a*c
 	L=Int64(floor(sqrt(sqrt(D/4))))
-	A=a/gcd(b,a)
-	B=b/gcd(b,a)
-	C=(-c*gcdx(b,a)[2])%A
+	(d1,u,v)=gcdx(b,a)
+	A=a/d1
+	B=b/d1
+	C=(-c*u)%A
 	C1=A-C
 	if C1<C
 		C=-C1
 	end
 	(z,d)=parteucl(A,C,L)
 	if z==0
-		g=(B+v3+c)/d
-		a2=d*d
-		c2=v3*v3
+		g=(B*v3+c)/d
+		a2=d^2
+		c2=v3^2
 		b2=b+(d+v3)^2-a2-c2
-		c2=c2+g*gcd(b,a)
-		return reduce([a2;b2;c2])
+		c2=c2+g*d1
+		return formreduce([a2;b2;c2])
 	end
 	e=(c*v+B*d)/A
 	g=(e*v2-B)/v
@@ -113,45 +142,58 @@ function nudupl(f::Array{Int64,1})
 	if d1>1
 		b2=d1*b2
 		v=d1*v
-		v2=a1*v2
+		v2=d1*v2
 	end
 	a2=d^2
 	c2=v3^2
 	b2=b2+(d+v3)^2-a2-c2
 	a2=a2+e*v
 	c2=c2+g*v2
-	return reduce([a2;b2;c2])
+	return formreduce([a2;b2;c2])
 end
 
-function nucomp(f::Array{Int64,1},g::Array{Int64,1})
-	a1=f[1]
-	b1=f[2]
-	c1=f[3]
-	a2=g[1]
-	b2=g[2]
-	c2=g[3]
-	D=b*b-4*a*c
-	L=Int64(floor(sqrt(sqrt(D/4))))
-	if a1<a2
-		x=f
-		f=g
-		g=x
+#julia> f1
+#3-element Array{Int64,1}:
+# 11
+#  2
+#  3
+#
+#julia> f2
+#3-element Array{Int64,1}:
+#  3
+# 2
+# 11
+function nucomp(f1::Array{Int64,1},f2::Array{Int64,1})
+	i=f1
+	h=f2
+	if f1[1]<f2[1]
+		x=f1
+		f1=f2
+		f2=x
 	end
-	s(b1+b2)/2
+	a1=f1[1]
+	b1=f1[2]
+	c1=f1[3]
+	a2=f2[1]
+	b2=f2[2]
+	c2=f2[3]
+	D=4*a1*c1-b1^2
+	L=Int64(floor(sqrt(sqrt(D/4))))
+	s=Int64((b1+b2)/2)
 	n=b2-s
 	(d,u,v)=gcdx(a1,a2)
 	if d==1
-		A=-gcdx(a1,a2)[2]*n
-		d1=1
+		A=-u*n
+		d1=d
 		A=A%a1
 		A1=a1-A
 		if A1<A
 			A=-A1
 		end
-		(z,d)=parteucl(a1,A)
+		(z,d)=parteucl(a1,A,L)
 	elseif s%d==0
-		A=-gcdx(a1,a2)[2]*n
-		d1=gcd(a1,a2)
+		A=-u*n
+		d1=d
 		a1=a1/d1
 		a2=a2/d1
 		s=s/d1
@@ -160,7 +202,7 @@ function nucomp(f::Array{Int64,1},g::Array{Int64,1})
 		if A1<A
 			A=-A1
 		end
-		(z,d)=parteucl(a1,A)
+		(z,d)=parteucl(a1,A,L)
 	else
 		(d1,u1,v1)=gcdx(s,d) 
 		if d1>1
@@ -174,36 +216,43 @@ function nucomp(f::Array{Int64,1},g::Array{Int64,1})
 		l=(-u1*(u*c1+v*c2))%d
 		A=-u*(n/d)+l*(a1/d)
 		A=A%a1
+		A1=a1-A
 		if A1<A
 			A=-A1
 		end
-		(z,d)=parteucl(a1,A)
+		(z,d)=parteucl(a1,A,L)
 	end
 	if z==0
 		Q1=a2*v3
 		Q2=Q1+n
-		h=Q2/d
-		i=(v3*s+c2)/d
+		f=Q2/d
+		g=(v3*s+c2)/d
 		a3=d*a2
 		c3=v3*d+g*d1
 		b3=2*Q1+b2
-		return reduce([a3;b3;c3])
+		f3=[a3;b3;c3]	
+		f1=i
+		f2=h
+		return formreduce(f3)
 	end
 	b=(a2*d+n*v)/a1
 	Q1=b*v3
 	Q2=Q1+n
 	f=Q2/d
-	e=(s*d+c2v)/a1
+	e=(s*d+c2*v)/a1
 	Q3=e*v2
 	Q4=Q3-s
 	g=Q4/v
-	if d1<1
+	if d1>1
 		v2=d1*v2
 		v=d1*v
 	end
 	a3=d*b+e*v
 	c3=v3*f+g*v2
 	b3=Q1+Q2+d1*(Q3+Q4)
-	return reduce([a3;b3;c3])
+	f3=[a3;b3;c3]
+	f1=i
+	f2=h
+	return formreduce(f3)
 end	
 	
